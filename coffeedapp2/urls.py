bd12c7b357956b8e6439404efcedda8e95c90d9d
Anders Hasselstrøm <nb@Ns-MacBook-Pro.local>
2015-04-03 22:06:33 -0700

    ready to push

--- coffeedapp2/urls.py
+++ coffeedapp2/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'coffeedapp2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+    (r'', include('core.urls')),
+)
+
+
